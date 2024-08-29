d223c917ded69c952b4505851e1f803f2761ff37
Jamarare <allanlond23@gmail.com>
2024-08-29 13:14:17 +0300

    .

--- ContosoUniversity/Controllers/StudentsController.cs
+++ ContosoUniversity/Controllers/StudentsController.cs
@@ -1,12 +1,70 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContosoUniversity.Controllers
 {
     public class StudentsController : Controller
     {
-        public IActionResult Index()
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
         {
-            return View();
+            _context = context;
         }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Students.ToListAsync());
+        }
+        /*public async Task<IActionResult> Index(
+            string sortOrder,
+            string currentFilter,
+            string searchString,
+            int? pageNumber
+            )
+        {
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["NameSortParam"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["DateSortParam"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewData["currentFilter"] = searchString;
+
+            var students = from student in _context.Students
+                           select student;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(student =>
+                student.LastName.Contains(searchString) ||
+                student.FirstMidName.Contains(searchString));
+            }
+            switch (sortOrder) 
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(student => student.LastName);
+                case "firstname_desc":
+                    students = students.OrderByDescending(student => student.FirstMidName);
+                case "Date":
+                    students = students.OrderBy(student => student.EnrollmentDate);
+                case "name_desc":
+                    students = students.OrderByDescending(student => student.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(student => student.LastName);
+                    break;
+            }
+
+            int pageSize = 3;
+            return View(await _context.Students.ToListAsync));
+        }*/  
     }
 }

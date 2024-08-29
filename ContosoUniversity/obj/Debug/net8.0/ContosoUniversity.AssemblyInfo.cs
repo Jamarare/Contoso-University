6b0603b30d00d21960ac7f33fc3e6dc52c0f4c7f
Jamarare <allanlond23@gmail.com>
2024-08-29 12:13:58 +0300

    .

--- ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f07da30c7aad4a6e28278d6a8c46494d81e5afa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34c690fbd74e2e36308711001020e97e65d95e43")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

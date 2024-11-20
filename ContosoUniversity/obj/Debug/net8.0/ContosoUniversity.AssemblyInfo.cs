be87ca5602a762eb5b9b0ae34947f2197757c533
Jamarare <allanlond23@gmail.com>
2024-11-20 20:34:58 +0200

    Added delete to courses

--- ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa9a2a03aa61960361bed4639564941afd18aff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204c160e62a5868cc42a4490be1580a074199bd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

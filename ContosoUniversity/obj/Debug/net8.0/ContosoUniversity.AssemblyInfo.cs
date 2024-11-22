a0fd3d68496eec2e7d47b1e47c6cac710c0f1990
opilane <opilane@HP-CZC3217070>
2024-11-22 12:23:23 +0200

    Added create edit

--- ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ ContosoUniversity/obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9aaeabe0d73351b7e80b34767cdd2a7373ed3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6acba93a34878bfc2a51e884fb1c5320053244a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
